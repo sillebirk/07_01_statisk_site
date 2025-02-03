018509607bb47472595cba7a0eb45b7a53e34faf
Sille Birk <sila0004@stud.kea.dk>
2025-02-03 13:54:20 +0100

    ret til

--- productlist.js
+++ productlist.js
@@ -1,24 +1,24 @@
 const productContainer = document.querySelector(".product_list_container");
 
-fetch(`https://kea-alt-del.dk/t7/api/products/`)
+fetch(`https://kea-alt-del.dk/t7/api/products?limit=100`)
   .then((response) => response.json())
   .then((data) => showList(data));
 
-function showList(product) {
-  console.log(product);
-  let markup = "";
-  product
-    .map((product) => {
-      markup += `<article>
+function showList(data) {
+  console.log(data);
+  const markup = data
+    .map(
+      (product) => `<article>
         <a href="product.html"><img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp"
         alt="${product.productdisplayname}"
         class="image_container"/></a>
         <h2>${product.productdisplayname}</h2>
         <p>Category | ${product.category}</p>
-        <p>${product.price} DKK</p>
+        <p>${product.price}</p>
         <a href="product.html" class="add_to_bag"><p>ADD TO BAG</p></a>
-        </article>`;
-    })
+        </article>
+        `
+    )
     .join("");
   console.log(markup);
   productContainer.innerHTML = markup;
